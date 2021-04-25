50bf4d485c1df435bdb9087024f9aeabdd2c159f
xiaonian <xiaonian0430@163.com>
2021-04-25 14:47:55 +0800

    1212

--- main.py
+++ main.py
@@ -13,42 +13,36 @@ import os
 import subprocess
 
 
-def init_dir():
-    try:
-        if not os.path.exists('./data'):
-            os.mkdir('./data')
-        if not os.path.exists('./Log'):
-            os.mkdir('./Log')
-        if not os.path.exists('./Temp'):
-            os.mkdir('./Temp')
-        return True
-    except:
-        return False
-
-
 def create_dir(path):
     os.makedirs(path, exist_ok=True)
 
 
-def run():
-    # 启动程序
-    print('start')
-    init_dir()
-
-
-if __name__ == "__main__":
-    # run()
-    target_path = 'm3u8/1001'
+def generate_m3u8(input_path, input_text):
+    target_path = 'public_job/m3u8/' + input_text
     target_seg_path = target_path + '/segment'
     create_dir(target_seg_path)
-    input_path = '1001.mp4'
-    mp4_path = target_path + '/1001.mp4'
-    ts_path = target_path + '/1001.ts'
-    m3u8_path = target_seg_path + '/1001.m3u8'
-    m3u8_segment_path = target_seg_path + '/1001_%03d.ts'
+    mp4_path = target_path + '/' + input_text + '.mp4'
+    ts_path = target_path + '/' + input_text + '.ts'
+    m3u8_path = target_seg_path + '/' + input_text + '.m3u8'
+    m3u8_segment_path = target_seg_path + '/' + input_text + '_%03d.ts'
     other_2_mp4 = 'ffmpeg -i ' + input_path + ' -c:v copy -c:a copy ' + mp4_path
     mp4_2_ts = 'ffmpeg -i ' + mp4_path + ' -c copy -bsf:v h264_mp4toannexb ' + ts_path
     ts_2_m3u8 = 'ffmpeg -i ' + ts_path + ' -c copy -map 0 -f segment -segment_time 1 -segment_list ' + m3u8_path + ' ' + m3u8_segment_path
     subprocess.call(other_2_mp4, shell=True)
     subprocess.call(mp4_2_ts, shell=True)
     subprocess.call(ts_2_m3u8, shell=True)
+
+
+def run():
+    # 启动程序
+    print('start')
+    input_dir = 'public_job/input'
+    for parent, dir_names, file_names in os.walk(input_dir):
+        for file_name in file_names:
+            input_path = input_dir + '/' + file_name
+            input_text = os.path.splitext(file_name)[0]
+            generate_m3u8(input_path, input_text)
+
+
+if __name__ == "__main__":
+    run()
